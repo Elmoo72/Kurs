205eb987d07bff745a59c1aacb4627119f72ccc1
Elmoo72 <makar3452@yandex.ru>
2025-05-08 00:48:39 +0300

    Исправлены критические ошибки, добавлен UI тест для convert и unit тест для array

--- MovieQuizUItests/MovieQuizUItests.swift
+++ MovieQuizUItests/MovieQuizUItests.swift
@@ -74,8 +74,33 @@ final class MovieQuizUItests: XCTestCase {
         let secondPosterData = secondPoster.screenshot().pngRepresentation
 
         let indexLabel = app.staticTexts["Index"]
-       
+        
         XCTAssertNotEqual(firstPosterData, secondPosterData)
+        XCTAssertEqual(indexLabel.label, "2/10")
+    }
+    
+    func testConvert() {
+        sleep(3)
+        
+       
+        let firstPoster = app.images["Poster"]
+        let firstPosterData = firstPoster.screenshot().pngRepresentation
+        
+      
+        app.buttons["Yes"].tap()
+        sleep(3)
+        
+        
+        let secondPoster = app.images["Poster"]
+        let secondPosterData = secondPoster.screenshot().pngRepresentation
+        
+        
+        let indexLabel = app.staticTexts["Index"]
+        
+        
+        XCTAssertNotEqual(firstPosterData, secondPosterData)
+        
+       
         XCTAssertEqual(indexLabel.label, "2/10")
     }
     
